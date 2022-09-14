ce9252236b4afd3eb5489cdbc7e92d9ca97e86b4
Mayank Agrawal <mayank@caizin.com>
2022-09-14 10:37:35 +0530

    Completed with the encryption and decryption

--- app/index.ts
+++ app/index.ts
@@ -31,6 +31,17 @@ document.addEventListener('DOMContentLoaded', () => {
         
         let output: string = encryptString(inputString, parseInt(key));
         console.log(output);
+
+        let inputElement: HTMLSpanElement = <HTMLSpanElement> document.getElementById('input');
+        let outputElement: HTMLSpanElement = <HTMLSpanElement> document.getElementById('output');
+        let resElement: HTMLParagraphElement = <HTMLParagraphElement> document.getElementById('result');
+
+        inputElement.innerHTML = inputString;
+        outputElement.innerHTML = output;
+        resElement.classList.remove('hide');
+
+        localStorage.setItem("input", output);
+        localStorage.setItem("key", key);
     });
 
     decryptButton.addEventListener('click', () => {
@@ -41,13 +52,81 @@ document.addEventListener('DOMContentLoaded', () => {
         
         let output: string = decryptString(inputString, parseInt(key));
         console.log(output);
+        
+        let inputElement: HTMLSpanElement = <HTMLSpanElement> document.getElementById('input');
+        let outputElement: HTMLSpanElement = <HTMLSpanElement> document.getElementById('output');
+        let resElement: HTMLParagraphElement = <HTMLParagraphElement> document.getElementById('result');
+
+        inputElement.innerHTML = inputString;
+        outputElement.innerHTML = output;
+        resElement.classList.remove('hide');
+
+        localStorage.setItem("input", output);
+        localStorage.setItem("key", key);
     });
 
     function encryptString(input: string, key: number): string {
-        return "hello";
+        let matrix: string[][] = [];
+        let i: number = 0;
+        let l: number = input.length;
+
+        // input = input.replace(/ /g,'#');
+
+        while(i < l) {
+            let arr: string[] = [];
+            for(let j:number = 0; j < key; j++) {
+                if(i == l) break;
+
+                arr.push(input.charAt(i));
+                i++;
+            }
+            matrix.push(arr);
+        }
+
+        console.log(matrix);
+
+        let result = "";
+        for(let j:number = 0; j < key; j++) {
+            for(let i:number = 0; i < matrix.length; i++) {
+                if(j >= matrix[i].length) break;
+                result += matrix[i][j];
+            }
+        }
+        return result;
     }
 
     function decryptString(input: string, key: number): string {
-        return "dello";
+        let newkey: number = Math.floor(input.length / key);
+        let rem: number = input.length % key;
+
+        let matrix: string[][] = [];
+        let i: number = 0;
+        let l: number = input.length;
+
+        while(i < l) {
+            let arr: string[] = [];
+            for(let j:number = 0; j < newkey + rem; j++) {
+                if(i == l) break;
+
+                arr.push(input.charAt(i));
+                i++;
+            }
+            if(rem > 0) rem--;
+            matrix.push(arr);
+        }
+
+        console.log(matrix);
+        rem = input.length % key;
+
+        let result: string = "";
+        for(let j:number = 0; j < newkey + rem; j++) {
+            for(let i:number = 0; i < matrix.length; i++) {
+                if(j >= matrix[i].length) break;
+                result += matrix[i][j];
+            }
+        }
+
+        // result = result.replace(/#/g,' ');
+        return result;
     }
 });
--- js/index.js
+++ js/index.js
@@ -22,6 +22,14 @@ document.addEventListener('DOMContentLoaded', () => {
             return;
         let output = encryptString(inputString, parseInt(key));
         console.log(output);
+        let inputElement = document.getElementById('input');
+        let outputElement = document.getElementById('output');
+        let resElement = document.getElementById('result');
+        inputElement.innerHTML = inputString;
+        outputElement.innerHTML = output;
+        resElement.classList.remove('hide');
+        localStorage.setItem("input", output);
+        localStorage.setItem("key", key);
     });
     decryptButton.addEventListener('click', () => {
         getInput();
@@ -29,12 +37,69 @@ document.addEventListener('DOMContentLoaded', () => {
             return;
         let output = decryptString(inputString, parseInt(key));
         console.log(output);
+        let inputElement = document.getElementById('input');
+        let outputElement = document.getElementById('output');
+        let resElement = document.getElementById('result');
+        inputElement.innerHTML = inputString;
+        outputElement.innerHTML = output;
+        resElement.classList.remove('hide');
+        localStorage.setItem("input", output);
+        localStorage.setItem("key", key);
     });
     function encryptString(input, key) {
-        return "hello";
+        let matrix = [];
+        let i = 0;
+        let l = input.length;
+        while (i < l) {
+            let arr = [];
+            for (let j = 0; j < key; j++) {
+                if (i == l)
+                    break;
+                arr.push(input.charAt(i));
+                i++;
+            }
+            matrix.push(arr);
+        }
+        console.log(matrix);
+        let result = "";
+        for (let j = 0; j < key; j++) {
+            for (let i = 0; i < matrix.length; i++) {
+                if (j >= matrix[i].length)
+                    break;
+                result += matrix[i][j];
+            }
+        }
+        return result;
     }
     function decryptString(input, key) {
-        return "dello";
+        let newkey = Math.floor(input.length / key);
+        let rem = input.length % key;
+        let matrix = [];
+        let i = 0;
+        let l = input.length;
+        while (i < l) {
+            let arr = [];
+            for (let j = 0; j < newkey + rem; j++) {
+                if (i == l)
+                    break;
+                arr.push(input.charAt(i));
+                i++;
+            }
+            if (rem > 0)
+                rem--;
+            matrix.push(arr);
+        }
+        console.log(matrix);
+        rem = input.length % key;
+        let result = "";
+        for (let j = 0; j < newkey + rem; j++) {
+            for (let i = 0; i < matrix.length; i++) {
+                if (j >= matrix[i].length)
+                    break;
+                result += matrix[i][j];
+            }
+        }
+        return result;
     }
 });
 //# sourceMappingURL=index.js.map
